416af874f7c648aeafc68ae7bf21494c5037ccd9
Lyov2006 <lyov.arsh2006@gmail.com>
2023-06-03 04:48:11 -0400

    third commit

---
+++ Desktop/cybersec.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+using namespace std;
+/*
+int addition(int a, int b) {
+	return a+b;
+}
+*/
+/*2
+int addition(int a) {
+return a+1;
+}
+*/
+
+/*3
+int convert(int minutes) {
+	return minutes*60;
+}
+*/
+
+/*4
+int triArea(int base, int height) {
+	return (base*height)/2;
+}
+*/
+
+/*5
+int circuitPower(int voltage, int current) {
+	return voltage*current;
+}
+*/
+
+/*6
+int howManySeconds(int hours) {
+	return hours*3600;
+}
+*/
+
+/*7
+int nextEdge(int side1, int side2) {
+	return side1+side2-1;
+}
+*/
+/*8
+int remainder(int x, int y) {
+	return x%y;
+}
+*/
+/*9
+int findPerimeter(int length, int width) {
+	return length*2+width*2;
+}
+*/
+/*10
+int findPerimeter(int length, int width) {
+	return length*2+width*2;
+}
+*/
+/*11
+bool isEqual(int num1, int num2) {
+	if(num1 == num2)
+		return true;
+	else
+		return false;
+}
+*/
+/*12
+bool lessThanOrEqualToZero(int num) {
+	if(num<=0){
+		return true;
+	}else{
+		return false;
+	}
+}
+*/
+/*13
+int animals(int chickens, int cows, int pigs) {
+	return chickens*2+cows*4+pigs*4;
+}
+*/
+/*14
+bool isEqual(int num1, int num2) {
+	if(num1 == num2)
+		return true;
+	else
+		return false;
+}
+*/
+/*15
+bool reverse(bool boolean) {
+	return !boolean;
+}
+*/
+/*16
+bool isSeven(int x){
+	return (x == 7) ? true : false;
+}
+*/
+/*17
+bool lessThan100(int a, int b) {
+	if(a+b<100){
+		return true;
+	}else{
+		return false;
+	}
+}
+*/
+/*18
+int convert(int hours, int minutes) {
+	return hours*3600+minutes*60;
+}
+*/
+/*19
+int sumPolygon(int n) {
+	return (n-2)*180;
+}
+*/
+/*20
+int points(twoPointers, threePointers) {
+	return twoPointers*2+threePointers*3;
+}
+*/
